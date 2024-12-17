5f7e12f18c275aa4ea7482d708f01463134b72f2
Rahmeen Zindani <rahmeenzindani@gmail.com>
2024-12-16 21:26:22 -0600

    Add files via upload

---
+++ citibikedata.py
@@ -0,0 +1,27 @@
+DATEDIFF('minute', [start_time], [end_time])
+
+DATEPART('hour', [start_time])
+
+IF [user_type] = 'Subscriber' THEN 'Member'
+ELSE 'Casual'
+END
+
+COUNT([start_station_name])
+
+DATEDIFF('minute', [start_time], [end_time])
+
+COUNT([start_station_name])
+
+
+
+DATEDIFF('minute', [start_time], [end_time])
+
+DATEPART('hour', [start_time])
+
+IF [user_type] = 'Subscriber' THEN 'Member'
+ELSE 'Casual'
+END
+
+COUNT([start_station_name])
+
+RANK(SUM([Trip Count]), 'desc')
